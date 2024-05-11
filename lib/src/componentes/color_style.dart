3c96ca76a927ad189639cd24ace1629b0606841a
Bruno Henrique Guimarães <bruno.guima212@gmail.com>
2024-05-10 21:16:55 -0300

    esquema de cores

---
+++ lib/src/componentes/color_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle {
+  static const primary = Color.fromRGBO(72, 163, 247, 1);
+  static const secondary = Color.fromRGBO(194, 194, 194, 1);
+  static const background = Color(0xf3f3f3f3);
+}
